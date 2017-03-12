382c00b8cdc1613151baa85e27d1ce8d0dfac640
Nagy Zsolt <stay.pista@gmail.com>
2017-03-12 10:56:09 +0100

    Feladat kiírás.

---
+++ Gyakorlofeladat-03-12/main.c
@@ -0,0 +1,45 @@
+/**
+ * main.c
+ *
+ * Created: 2017. 03. 12. 10:38:32
+ * Author : Nagy Zsolt
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /** 1. Feladat:
+     *  Renedelkezésre áll egy "date" tömb ami egy dátumot tartalmaz!
+     *  A dátum egyes részeit írja ki a képernyõre így: 1970 (szóköz) 03 (szóköz) 12!
+     *  A pontok ne legyenek benne a kiiratásban.
+     *  Célidõ: 5 perc
+     */
+    date[20] = {"1970. 03. 12."};
+
+    /** 2. Feladat:
+     *  Készítsen egy függvényt ami "date" tömböt felülírja "currentdate" tömbel.
+     *  Célidõ: 10 perc
+     */
+    currentdate[] = {"2017. 03. 12."};
+
+    /** 3. Feladat:
+     *  Írja ki a képernyõre "date" tömböt angolszász formátumban (nap/hónap/év).
+     *  Célidõ: 10 perc
+     */
+
+     /** 4. Feladat:
+     *  Készítsen egy struktúra tipust, mely külön tárolja a nap hónap év elemeket.
+     *  És hozzon létre egy példányt az adott strukturából!
+     *  Célidõ: 15 perc
+     */
+
+     /** 5. Feladat:
+     *  Az elõzõ feladatban példányosított strukturát töltse ki a "currentdate" tömben tárolt elemekkel.
+     *  Majd irassa ki a dátumot a strukurából.
+     *  Célidõ: 10 perc
+     */
+
+    return 0;
+}
